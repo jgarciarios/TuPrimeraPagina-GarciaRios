5bad2ff4df053c10506d3e80e82ce43f6abec623
juan ignacio garcia <juanignaciogarciarios@gmail.com>
2024-12-07 15:26:49 -0300

    Actualización de vistas, modelos y plantillas en AppCoder

--- AppCoder/tests.py
+++ AppCoder/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Estudiante
 
-# Create your tests here.
+class EstudianteModelTest(TestCase):
+    def test_create_estudiante(self):
+        estudiante = Estudiante.objects.create(nombre="Juan", apellido="García", email="juan@correo.com")
+        self.assertEqual(estudiante.nombre, "Juan")
+        self.assertEqual(estudiante.apellido, "García")
+        self.assertEqual(estudiante.email, "juan@correo.com")
+
